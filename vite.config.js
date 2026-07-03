2429f229ee9c1b3c63f3bc5ec83779092cb50ff9
Pranav Turlapati <pran@unc.edu>
2026-07-03 17:05:38 -0400

    vite config changes

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default {
-  base: '/', // or your repo name if deploying to user.github.io/repo
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-  }
-}
+export default defineConfig({
+  base: '/', // root path for Vercel custom domain
+  plugins: [react()],
+})
